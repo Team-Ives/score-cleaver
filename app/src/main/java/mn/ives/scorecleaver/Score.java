816d58c84b5c024c33bf347925de0255fb8324c8
Courtney Ives <cgives@me.com>
2016-07-04 15:47:27 -0500

    Add new views to app, continued setup

---
+++ app/src/main/java/mn/ives/scorecleaver/Score.java
@@ -0,0 +1,7 @@
+package mn.ives.scorecleaver;
+
+/**
+ * Created by Courtney on 7/4/16.
+ */
+public class Score {
+}
--- app/src/main/java/mn/ives/scorecleaver/SetupActivity.java
+++ app/src/main/java/mn/ives/scorecleaver/SetupActivity.java
@@ -1,13 +1,31 @@
 package mn.ives.scorecleaver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SetupActivity extends AppCompatActivity {
+    public final static String EXTRA_SCORE = "mn.ives.scorecleaver.SCORE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+    }
+
+    /**
+     * Called when the user clicks the send button.
+     *
+     * @param view
+     */
+    public void startGame(View view){
+        Intent intent = new Intent(this, DisplayScoreActivity.class);
+        EditText editText = (EditText) findViewById(R.id.startScore);
+        String scoreString = editText.getText().toString();
+        Integer startScore = Integer.parseInt(scoreString);
+        intent.putExtra(EXTRA_SCORE, startScore.intValue());
+        startActivity(intent);
     }
 }
