0e085e671634661da9c6e8e1e1e6939c27e42db3
Courtney Ives <cgives@me.com>
2016-07-10 20:58:02 -0500

    issue 18: Implement player score saving

--- app/src/main/java/mn/ives/scorecleaver/PlayerScore.java
+++ app/src/main/java/mn/ives/scorecleaver/PlayerScore.java
@@ -1,26 +1,43 @@
 package mn.ives.scorecleaver;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.TextView;
 
 /**
  * Used to keep track of player scores
  */
 public class PlayerScore {
+    public static int RESTORE_SCORES = -1;
+
     private int currentScore;
     private int playerId;
+    private String playerUid;
     private AbstractScoreActivity activity;
 
     public PlayerScore(AbstractScoreActivity activity, int playerId, int startingScore) {
-        this.currentScore = startingScore;
         this.activity = activity;
         this.playerId = playerId;
+        this.playerUid = activity.getString(R.string.player_prefix) + String.valueOf(playerId);
 
-        this.setActivityScore(startingScore);
+        if(startingScore == RESTORE_SCORES) {
+            SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+            this.currentScore = sharedPref.getInt(this.playerUid, startingScore);;
+        } else {
+            this.currentScore = startingScore;
+        }
+
+        this.setActivityScore(this.currentScore);
     }
 
     public void setActivityScore(int score){
         TextView textView = (TextView) this.activity.findViewById(this.playerId);
         textView.setText(String.valueOf(score));
+
+        SharedPreferences sharedPref = this.activity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(playerUid, score);
+        editor.apply();
     }
 
     public int currentHealth(){
--- app/src/main/java/mn/ives/scorecleaver/SetupActivity.java
+++ app/src/main/java/mn/ives/scorecleaver/SetupActivity.java
@@ -1,6 +1,8 @@
 package mn.ives.scorecleaver;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,9 +20,14 @@ public class SetupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
-        TypefaceHelper.applyAudiowide(this, R.id.startScore, R.id.numPlayersLabel, R.id.startScoreLabel);
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = getResources().getString(R.string.default_score);
+        String lastValue = sharedPref.getString(getString(R.string.last_starting_score), defaultValue);
+
+        TypefaceHelper.applyAudiowide(this, R.id.startScore, R.id.numPlayersLabel, R.id.startScoreLabel, R.id.resumeLabel);
 
         EditText startScoreTextEdit = (EditText)findViewById(R.id.startScore);
+        startScoreTextEdit.setText(lastValue);
         startScoreTextEdit.setSelection(startScoreTextEdit.getText().length());
     }
 
@@ -34,6 +41,7 @@ public class SetupActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.startScore);
         String scoreString = editText.getText().toString();
         log.info(scoreString);
+        this.storeLastStart(scoreString, 2);
         intent.putExtra(EXTRA_SCORE, scoreString);
         startActivity(intent);
     }
@@ -48,9 +56,37 @@ public class SetupActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.startScore);
         String scoreString = editText.getText().toString();
         log.info(scoreString);
+        this.storeLastStart(scoreString, 4);
         intent.putExtra(EXTRA_SCORE, scoreString);
         startActivity(intent);
     }
 
+    /**
+     * Called when the user clicks the resume button.
+     *
+     * @param view
+     */
+    public void resumeGame(View view){
+        Intent intent;
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        int lastPlayers = sharedPref.getInt(getString(R.string.last_starting_players), 2);
+
+        if(lastPlayers == 2) {
+            intent = new Intent(this, DisplayTwoPlayerScoreActivity.class);
+        } else {
+            intent = new Intent(this, DisplayFourPlayerScoreActivity.class);
+        }
+        intent.putExtra(EXTRA_SCORE, String.valueOf(PlayerScore.RESTORE_SCORES));
+        startActivity(intent);
+    }
+
+    private void storeLastStart(String value, int startingPlayers){
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.last_starting_score), value);
+        editor.putInt(getString(R.string.last_starting_players), startingPlayers);
+        editor.apply();
+    }
 
 }
